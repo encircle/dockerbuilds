84dae17f285831bc275fe6fd9197ad27100aaf03
encircle <support@encircle.co.uk>
2020-05-18 13:49:10 +0100

    modified letsencrypt loader script

--- entry/webserver/entrypoint.sh
+++ entry/webserver/entrypoint.sh
@@ -1,11 +1,12 @@
-function install_client {
+function install_client()
+{
   cd /tmp && git clone https://github.com/Neilpang/acme.sh.git
   cd acme.sh && ./acme.sh --install
-  source ~/.bashrc
 }
 
-function setup {
-  mkdir /var/www/html/.well-known/acme-challenge
+function setup()
+{
+  mkdir -p /var/www/html/.well-known/acme-challenge
   chown -R 101:82 /var/www/html/.well-known/acme-challenge
   chmod -R 0555 /var/www/html/.well-known/acme-challenge
 
@@ -13,21 +14,27 @@ function setup {
   openssl dhparam -dsaparam -out /etc/nginx/ssl/letsencrypt/$SITE/dhparams.pem 4096
 }
 
-function request_cert {
-  acme.sh --issue -w /var/www/html -d $SITE -k 4096apk add netcat-openbsd bc curl wget git bash openssl
-  acme.sh --issue -w /var/www/html -d $SITE -k 4096
+function request_cert()
+{
+  cd /root/.acme.sh
+  ./acme.sh --issue -w /var/www/html -d $SITE -k 4096 --debug
 }
 
-function install_cert {
-  acme.sh --installcert -d $SITE \
+function install_cert()
+{
+  cd /root/.acme.sh
+  ./acme.sh --installcert -d $SITE \
     --keypath /etc/nginx/ssl/letsencrypt/$SITE/$SITE.key \
     --fullchainpath /etc/nginx/ssl/letsencrypt/$SITE/$SITE.cer \
     --reloadcmd '/etc/init.d/nginx restart'
 }
 
-main {
+main()
+{
   install_client
   setup
   request_cert
   install_cert
 }
+
+main
