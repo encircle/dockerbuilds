391ec1e8a69d6ffa4bb44d66ccea893c4a1861f4
matt <pagey101@hotmail.co.uk>
2020-05-29 14:56:40 +0100

    added images for nginx with modsec and clamav

---
+++ software/clamav-hook.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+   
+$CLAMSCAN = "/usr/bin/clamscan";
+   
+if (@ARGV != 1) {
+    print "Usage: modsec-clamscan.pl FILENAME\n";
+    exit;
+}
+   
+my ($FILE) = @ARGV;
+   
+$cmd = "$CLAMSCAN --stdout --disable-summary $FILE";
+$input = `$cmd`;
+$input =~ m/^(.+)/;
+$error_message = $1;
+   
+$output = "0 Unable to parse clamscan output";
+   
+if ($error_message =~ m/: Empty file\.$/) {
+    $output = "1 empty file";
+}
+elsif ($error_message =~ m/: (.+) ERROR$/) {
+    $output = "0 clamscan: $1";
+}
+elsif ($error_message =~ m/: (.+) FOUND$/) {
+    $output = "0 clamscan: $1";
+}
+elsif ($error_message =~ m/: OK$/) {
+    $output = "1 clamscan: OK";
+}
+   
+print "$output\n";
