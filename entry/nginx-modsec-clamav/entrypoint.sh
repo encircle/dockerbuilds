46db4f8b7c3dfe0e325dc666f7777ae7b5bfb9ec
matt <pagey101@hotmail.co.uk>
2020-06-12 11:55:50 +0100

    added files

---
+++ entry/nginx-modsec-clamav/entrypoint.sh
@@ -0,0 +1,65 @@
+function start_nginx() {
+  nginx -g 'daemon off;' &
+}
+
+function install_packages() {
+  apk add netcat-openbsd bc curl wget git bash openssl libressl;
+}
+
+function install_client()
+{
+  rm -rf /tmp/acme.sh
+  cd /tmp && git clone https://github.com/Neilpang/acme.sh.git
+  cd acme.sh && ./acme.sh --install
+}
+
+function setup()
+{
+  mkdir -p /var/www/html/.well-known/acme-challenge
+  chown -R 101:82 /var/www/html/.well-known/acme-challenge
+  chmod -R 0555 /var/www/html/.well-known/acme-challenge
+
+  mkdir -p /etc/nginx/ssl/letsencrypt/$SITE
+  openssl dhparam -dsaparam -out /etc/nginx/ssl/letsencrypt/$SITE/dhparams.pem 4096
+}
+
+function get_cert()
+{
+  cd /root/.acme.sh
+  ./acme.sh --issue -w /var/www/html -d $SITE \
+    --keylength 4096 \
+    --cert-file /etc/nginx/certs/site.crt \
+    --key-file /etc/nginx/certs/site.key \
+    --fullchain-file /etc/nginx/certs/fullchain.crt \
+    --debug 
+}
+
+function letsencrypt()
+{
+  if [[ "$LETSENCRYPT" == "YES" ]]; then
+    if [[ ! -f /tmp/.gotcert ]]; then
+      rm -rf /var/www/.well-known
+      start_nginx
+      install_packages
+      install_client
+      setup
+      get_cert
+      nginx -s stop
+      touch /tmp/.gotcert
+    fi
+  fi
+}
+
+function clamav() {
+  freshclam -d &
+  clamd &
+}
+
+function main() {
+  set -e
+  letsencrypt
+  clamav
+  nginx -g 'daemon off;'
+}
+
+main
