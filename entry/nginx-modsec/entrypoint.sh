342c26189023eca9a41cb809e574937d735a897e
matt <pagey101@hotmail.co.uk>
2020-07-27 12:56:17 +0100

    removed letsencrypt from nginx image, better served as a seperate container

--- entry/nginx-modsec/entrypoint.sh
+++ entry/nginx-modsec/entrypoint.sh
@@ -1,39 +1,3 @@
-function start_nginx() {
-  nginx -g 'daemon off;' &
-}
-
-function install_packages() {
-  apk add netcat-openbsd bc curl wget git bash openssl libressl;
-}
-
-function install_client()
-{
-  rm -rf /tmp/acme.sh
-  cd /tmp && git clone https://github.com/Neilpang/acme.sh.git
-  cd acme.sh && ./acme.sh --install
-}
-
-function setup()
-{
-  mkdir -p /var/www/html/.well-known/acme-challenge
-  chown -R 101:82 /var/www/html/.well-known/acme-challenge
-  chmod -R 0555 /var/www/html/.well-known/acme-challenge
-
-  mkdir -p /etc/nginx/ssl/letsencrypt/$SITE
-  openssl dhparam -dsaparam -out /etc/nginx/ssl/letsencrypt/$SITE/dhparams.pem 4096
-}
-
-function get_cert()
-{
-  cd /root/.acme.sh
-  ./acme.sh --issue -w /var/www/html -d $SITE \
-    --keylength 4096 \
-    --cert-file /etc/nginx/certs/domain.crt \
-    --key-file /etc/nginx/certs/site.key \
-    --fullchain-file /etc/nginx/certs/site.crt \
-    --debug 
-}
-
 function env_sub()
 {
   envsubst '${SITE},${FPM_HOST}' < /etc/nginx/conf.d/default.conf > /tmp/default.conf && mv /tmp/default.conf /etc/nginx/conf.d/default.conf
@@ -113,7 +77,6 @@ function custom_errors()
 function main() {
   set -e
   env_sub
-  letsencrypt
   htpasswd
   log_permissions
   basic_auth_whitelist
