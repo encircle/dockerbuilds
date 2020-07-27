93ee19384ca862c54f08529b2e01dbc48b339712
matt <pagey101@hotmail.co.uk>
2020-07-27 12:39:01 +0100

    drupal updates

--- entry/drupal/entrypoint.sh
+++ entry/drupal/entrypoint.sh
@@ -6,25 +6,32 @@
 
 set -eux
 
+# Wait for database as drush requires it
 /usr/local/bin/wait-for -t 10 $DB_HOST:3306 &&
 
 (
   set -eux
 
+  # Get installed version, and intended version (from image)
   volume_version=$(drush status | grep 'Drupal version' | awk '{print $4}')
   image_version=$DRUPAL_VERSION
 
+  # if an update is required
   if [[ $volume_version != $image_version ]]; then
 
-    echo "Updating Drupal from $installed_version to $image_version"
+    echo "Updating Drupal from $volume_version to $image_version"
 
+    # Get core
     curl -fSL "https://ftp.drupal.org/files/projects/drupal-${DRUPAL_VERSION}.tar.gz" -o drupal.tar.gz
     echo "${DRUPAL_MD5} *drupal.tar.gz" | md5sum -c -
     tar -xz --strip-components=1 -f drupal.tar.gz
     rm drupal.tar.gz
     
+    # Set permissions
     /usr/local/bin/permissions.sh
 
+    # Update DB
+    yes | drush updatedb
   fi 
 ) || echo 'No database connection, cannot determine current Drupal version. Update aborted!'
 
