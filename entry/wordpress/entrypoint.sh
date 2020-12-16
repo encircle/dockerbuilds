d086da91fa0013d1de0fe9c437a59f69635add38
matt <pagey101@hotmail.co.uk>
2020-12-16 14:29:11 +0000

    added sender email as noreply@${SITE} to PHP postfix.ini config

--- entry/wordpress/entrypoint.sh
+++ entry/wordpress/entrypoint.sh
@@ -1,5 +1,7 @@
 set -x
 
+envsubst '${SITE}' < /usr/local/etc/php/conf.d/postfix.ini > /tmp/postfix.ini && mv /tmp/postfix.ini /usr/local/etc/php/conf.d/postfix.ini
+
 # Only do this is Wordpress is already installed
 if [[ -f /var/www/html/wp-config.php ]]; then
 
