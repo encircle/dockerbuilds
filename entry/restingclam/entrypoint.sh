3ad2cf5143629e1cd2c1d4a99c349b0de158959e
matt <pagey101@hotmail.co.uk>
2021-05-28 09:34:35 +0100

    restingclam moved to seperate repo

--- entry/restingclam/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-echo "Starting freshclam"
-freshclam -d &
-
-echo "Starting clamd"
-clamd &
-
-echo "Starting gunicorn"
-gunicorn --bind=unix:/var/run/gunicorn/gunicorn.sock --workers=3 --threads=3 rest:app
