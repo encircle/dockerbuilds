02c5f50b717226f86faf670327ca165dd4fb4e0a
matt <pagey101@hotmail.co.uk>
2021-08-12 16:43:19 +0100

    ssh changes

--- images/ssh/entrypoint.sh
+++ images/ssh/entrypoint.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 function main() {
-    echo 'Generating user key'
-    su encirsh -c "ssh-keygen -t rsa -f /home/${USER}/.ssh/id_rsa -q -P ''"
-    su encirsh -c "cat /home/${USER}/.ssh/id_rsa.pub > /home/${USER}/.ssh/authorized_keys"
-    echo 'Key generated and added to authorized_keys'
-    cat /home/${USER}/.ssh/id_rsa
-    /usr/sbin/sshd -D
+
+    if [ ! -f /home/${USER}/.ssh/id_rsa ]; then
+
+      echo 'Generating user key'
+      su encirsh -c "ssh-keygen -t rsa -f /home/${USER}/.ssh/id_rsa -q -P ''"
+      su encirsh -c "cat /home/${USER}/.ssh/id_rsa.pub > /home/${USER}/.ssh/authorized_keys"
+      echo 'Key generated and added to authorized_keys'
+      cat /home/${USER}/.ssh/id_rsa
+
+    fi
+
+    strace /usr/sbin/sshd -ddd -D
+
 }
 
 main
