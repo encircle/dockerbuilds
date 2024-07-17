9fa3dd7ed6a6d4bbafaa3e021510805c78db5108
William Chalmers <wchalmers@encircle.co.uk>
2024-07-17 12:16:47 +0100

    Drupal 7 updated to 7.99
    Drupal 7 permissions script changes

--- images/php/drupal7/permissions.sh
+++ images/php/drupal7/permissions.sh
@@ -3,18 +3,18 @@
 chown root:www-data /var/www/html/
 chmod 750 /var/www/html/
 find /var/www/html/ -maxdepth 1 -type f -exec chown root:www-data {} \;
-find /var/www/html/ -maxdepth 1 -type f -exec chmod 740 {} \;
+find /var/www/html/ -maxdepth 1 -type f ! -perm 0740 -exec chmod 740 {} \;
 
 # Generic
 find /var/www/html/ -exec chown root:www-data {} \;
-find /var/www/html/ -type d -exec chmod 750 {} \;
-find /var/www/html/ -type f -exec chmod 640 {} \;
+find /var/www/html/ -type d ! -perm 0750 -exec chmod 750 {} \;
+find /var/www/html/ -type f ! -perm 0640 -exec chmod 640 {} \;
 
 # Git
 find /var/www/html/.git -exec chown root:root {} \;
-find /var/www/html/.git -type d -exec chmod 750 {} \;
-find /var/www/html/.git -type f -exec chmod 640 {} \;
+find /var/www/html/.git -type d ! -perm 0750 -exec chmod 750 {} \;
+find /var/www/html/.git -type f ! -perm 0640 -exec chmod 640 {} \;
 
 # /sites/default/files
-find /var/www/html/sites/default/files -type d -exec chmod 770 {} \;
-find /var/www/html/sites/default/files -type f -exec chmod 660 {} \;
+find /var/www/html/sites/default/files -type d ! -perm 0770 -exec chmod 770 {} \;
+find /var/www/html/sites/default/files -type f ! -perm 0660 -exec chmod 660 {} \;
